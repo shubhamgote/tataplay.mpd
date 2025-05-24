3b3cf9611fe643b551c0f53af5d48f8d1f7be834
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-05-24 16:40:39 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const { id } = req.query;
   if (!id) return res.status(400).send("Missing ID");
 
-  const target = `http://amit-1.allinonereborn.in/tplaydrm19/manifest.mpd?id=${id}`;
+  const target = `http://amit-1.allinonereborn.in/tplaydrm/manifest.mpd?id=${id}`;
   res.writeHead(302, { Location: target });
   res.end();
 }
