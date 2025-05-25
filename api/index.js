c8269a4453b40ced4f08d758d79198d46e1b44ca
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-05-25 14:57:51 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,8 +9,7 @@ export default function handler(req, res) {
   }
 
   // Redirect to the manifest.mpd with the provided ID
-  // const target = `http://amit-1.allinonereborn.in/tplaydrm19/manifest.mpd?id=${id}`;
-  const target = `http://m3u.denver1769.fun/Play/TPv3.php?id=${id}`;
+  const target = `http://amit-1.allinonereborn.in/tplaydrm19/manifest.mpd?id=${id}`;
   res.writeHead(302, { Location: target });
   res.end();
 }
